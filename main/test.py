2ae9f3451e561b48724c8643ea5cd30f3920b8ce
LukasMoninger <lukasmoninger@gmail.com>
2025-06-25 14:50:38 +0200

    Add test for light sensor

--- main/test.py
+++ main/test.py
@@ -1,6 +1,7 @@
 import time
 import grovepi
 
+light_sensor = 0
 red_led = 2
 green_led = 3
 ultrasonic = 4
@@ -40,8 +41,23 @@ def test_ultrasonic():
         print(f"I/O-Error: {e}")
 
 
+def test_brightness():
+    interval = 0.5
+    duration = 10
+    start_time = time.time()
+
+    try:
+        while time.time() - start_time < duration:
+            brightness = grovepi.analogRead(light_sensor)
+            print(f"Brightness: {brightness}")
+            time.sleep(interval)
+    except IOError as e:
+        print(f"I/O-Error: {e}")
+
+
 if __name__ == "__main__":
     setup()
     test_led(red_led)
     test_led(green_led)
     test_ultrasonic()
+    test_brightness()
