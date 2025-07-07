93aafa2d3826637c8031402dcbc80ecf53ff9a52
LukasMoninger <lukasmoninger@gmail.com>
2025-07-07 21:15:10 +0200

    Added status variables for leds

--- main/actuators.py
+++ main/actuators.py
@@ -5,15 +5,17 @@ status_green_led = 0
 status_red_led = 0
 
 
-def switch_light_green(self):
+def switch_light_green():
+    global status_green_led, status_red_led
     grovepi.digitalWrite(red_led, 0)
-    self.status_red_led = 0
+    status_red_led = 0
     grovepi.digitalWrite(green_led, 1)
-    self.status_green_led = 1
+    status_green_led = 1
 
 
-def switch_light_red(self):
+def switch_light_red():
+    global status_green_led, status_red_led
     grovepi.digitalWrite(green_led, 0)
-    self.status_green_led = 0
+    status_green_led = 0
     grovepi.digitalWrite(red_led, 1)
-    self.status_red_led = 1
+    status_red_led = 1
