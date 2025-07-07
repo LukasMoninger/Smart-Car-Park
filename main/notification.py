212e1245beded601e7636eee6432c13fc6349500
LukasMoninger <lukasmoninger@gmail.com>
2025-07-07 18:19:18 +0200

    Fixed bugs in planning

--- main/notification.py
+++ main/notification.py
@@ -8,13 +8,13 @@ TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 TWILIO_PHONE_SENDER = "+15137177026"
 TWILIO_PHONE_RECIPIENT = "+4915757086879"
 
-def send_text_alert(alert_str):
+
+def send_text_notification(text):
     print("Using account SID:", TWILIO_ACCOUNT_SID)
     print("Using auth token:", TWILIO_AUTH_TOKEN)
 
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    message = client.messages.create(
+    client.messages.create(
         to=TWILIO_PHONE_RECIPIENT,
         from_=TWILIO_PHONE_SENDER,
-        body="Your text message here")
-    print(message.sid)
+        body=text)
--- main/test.py
+++ main/test.py
@@ -1,12 +1,22 @@
 import time
 import grovepi
 import subprocess
+import os
+import re
+
+from main.notification import send_text_notification
 from notification import send_text_alert
 
 light_sensor = 0
 red_led = 2
 green_led = 3
 ultrasonic = 4
+
+test_led_flag = False
+test_ultrasonic_flag = False
+test_brightness_flag = False
+test_messaging_flag = False
+test_pddl_flag = True
 
 
 def setup():
@@ -57,23 +67,51 @@ def test_brightness():
         print(f"I/O-Error: {e}")
 
 
-def generate_plan(domain, problem):
-    #cmd = ["pyperplan", domain, problem]
-    cmd = ["pyperplan", "-h", "debug", domain, problem]
-    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+def test_messaging():
+    text = "Test message from GrovePi"
+    send_text_notification(text)
+    print("Message sent:", text)
 
-    print(result.stdout)
 
-    lines = result.stdout.splitlines()
+def test_pddl():
+    domain = "../pddl/test/domain_example1.pddl"
+    problem = "../pddl/test/problem_example1.pddl"
+    plan = test_generate_plan(domain, problem)
+    print("Generated Plan of length ", len(plan))
+    print("Plan:", plan)
+    test_execute_plan(plan)
+
+
+def test_generate_plan(domain, problem):
+    timeout = 5
+    cmd = ["pyperplan", domain, problem]
+    solution_file = os.path.splitext(domain)[0] + ".soln"
+
+    if os.path.exists(solution_file):
+        os.remove(solution_file)
+
+    subprocess.run(cmd, check=True)
+    start = time.time()
+    while not os.path.exists(solution_file):
+        if time.time() - start > timeout:
+            raise RuntimeError(f"Timeout: '{solution_file}' not found within {timeout} seconds.")
+        time.sleep(0.1)
+
     plan = []
-    for line in lines:
-        if line.strip().startswith("1:"):
-            action = line.split(":", 1)[1].split("[")[0].strip()
-            plan.append(action)
+    with open(solution_file, "r") as file:
+        for line in file:
+            line = line.strip()
+            print(line)
+            if not line or line.startswith(";"):
+                continue
+            m = re.match(r'\s*\d+:\s*\(?(.+?)\)?\s*\[\d+]', line)
+            if m:
+                action = f"({m.group(1)})"
+                plan.append(action)
     return plan
 
 
-def execute_plan(plan):
+def test_execute_plan(plan):
     for step in plan:
         print("Execute:", step)
         name, *args = step.strip("()").split()
@@ -86,28 +124,17 @@ def move(robot, src, dst):
     print(f"Robot {robot} moves from {src} to {dst}")
 
 
-def test_pddl():
-    domain = "../pddl/domain_example.pddl"
-    problem = "../pddl/problem_example.pddl"
-    plan = generate_plan(domain, problem)
-
-    print("Generated Plan of length", len(plan))
-    print("Plan:", plan)
-
-    execute_plan(plan)
-
-
-def test_messaging():
-    alert_str = "Test message from GrovePi"
-    send_text_alert(alert_str)
-    print("Message sent:", alert_str)
-
-
 if __name__ == "__main__":
     setup()
-    # test_led(red_led)
-    # test_led(green_led)
-    # test_ultrasonic()
-    # test_brightness()
-    test_pddl()
-    # test_messaging()
+
+    if test_led_flag:
+        test_led(green_led)
+        test_led(red_led)
+    if test_ultrasonic_flag:
+        test_ultrasonic()
+    if test_brightness_flag:
+        test_brightness()
+    if test_messaging_flag:
+        test_messaging()
+    if test_pddl_flag:
+        test_pddl()
