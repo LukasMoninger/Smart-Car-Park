210d715cc14e325168a77c73106af58c8a3f0575
LukasMoninger <lukasmoninger@gmail.com>
2025-07-16 21:39:16 +0200

    Bug fix

--- main/test.py
+++ main/test.py
@@ -176,10 +176,31 @@ def on_message(client, userdata, msg):
 
 
 def test_led_strip():
-    strip = Adafruit_NeoPixel(TOTAL_LED_COUNT, 21, 800000, 5, False, 255)
+    LED_COUNT = 30  # Number of LED pixels.
+    LED_PIN = 21  # GPIO pin connected to the pixels (21 uses PWM)
+    LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800kHz)
+    LED_DMA = 10  # DMA channel to use for generating signal (try 10)
+    LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+    LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+    LED_CHANNEL = 0  # Set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+    # Create PixelStrip object with appropriate configuration.
+    strip = PixelStrip(
+        LED_COUNT,
+        LED_PIN,
+        LED_FREQ_HZ,
+        LED_DMA,
+        LED_INVERT,
+        LED_BRIGHTNESS,
+        LED_CHANNEL
+    )
+    # Initialize the library (must be called once before other functions).
     strip.begin()
-    strip.setPixelColorRGB(LED_CHIP_NUMBER, R, G, B)
-    strip.show()
+
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(255, 0, 0))
+        strip.show()
+        time.sleep(5)
 
 
 if __name__ == "__main__":
