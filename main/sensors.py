1f5d4d53d332f9c75f834611629eaa98ffef3bc8
LukasMoninger <lukasmoninger@gmail.com>
2025-07-17 23:45:33 +0200

    Test

--- main/sensors.py
+++ main/sensors.py
@@ -5,43 +5,23 @@ class Sensors:
 
     def __init__(self, mqtt_controller):
         self.mqtt_controller = mqtt_controller
-        self.brightness_sensor = 0
-        self.ultrasonic = 4
-        self.button = 5
-
-        grovepi.pinMode(self.button, "INPUT")
-
+        self.brightness_sensor_port = 0
+        self.ultrasonic_entrance_port = 4
+        self.button_port = 5
         self.brightness_limit = 200
-        self.distance_limit = 10
         self.co2_limit = 800
-
-    def read_brightness(self):
-        try:
-            brightness = grovepi.analogRead(self.brightness_sensor)
-            return brightness
-        except IOError as e:
-            print(f"I/O-Error: {e}")
+        self.distance_limit = 10
+        #grovepi.pinMode(self.button_port, "INPUT")
 
     def get_status_brightness(self):
-        brightness = self.read_brightness()
-        if brightness > self.brightness_limit:
-            return True
-        else:
-            return False
-
-    def read_ultrasonic(self):
         try:
-            distance = grovepi.ultrasonicRead(self.ultrasonic)
-            return distance
+            brightness = grovepi.analogRead(self.brightness_sensor_port)
+            if brightness > self.brightness_limit:
+                return True
+            else:
+                return False
         except IOError as e:
             print(f"I/O-Error: {e}")
-
-    def get_status_entrance(self):
-        distance = self.read_ultrasonic()
-        if distance < self.distance_limit:
-            return True
-        else:
-            return False
 
     def get_status_co2(self):
         co2_level = self.mqtt_controller.co2_level
@@ -50,15 +30,25 @@ class Sensors:
         else:
             return False
 
+    def get_status_entrance(self):
+        try:
+            distance = grovepi.ultrasonicRead(self.ultrasonic_entrance_port)
+            if distance < self.distance_limit:
+                return True
+            else:
+                return False
+        except IOError as e:
+            print(f"I/O-Error: {e}")
+
     def get_status_button(self):
         try:
-            state = grovepi.digitalRead(self.button)
+            state = grovepi.digitalRead(self.button_port)
             if state == 1:
                 return True
             else:
                 return False
-        except IOError:
-            print("I/O-Error while reading button state")
+        except IOError as e:
+            print(f"I/O-Error: {e}")
 
     def get_parking_occupancy(self, parking_space):
         if parking_space == 1:
